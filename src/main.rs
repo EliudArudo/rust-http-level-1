75f756e6217a948a1f91803a29070d81860d31f7
EliudArudo <eliudarudo@gmail.com>
2019-07-01 19:08:30 +0300

    First commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+mod routes;
+
+fn main() {
+    rocket::ignite().mount("/", routes![routes::index]).launch();
+}
+
